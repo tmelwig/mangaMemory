5561cedede34678ecfafb7c819b0d6253ab72593
Thomas Melwig <thomas.melwig@student-cs.fr>
2022-12-31 20:05:56 +0100

    no progress

--- js/main.js
+++ js/main.js
@@ -9,15 +9,17 @@ let Lien = document.getElementById("lien").value;
 let Url = document.getElementById("url").value;
 
 
+
 if(Lien=="" || Titre=="" || Url==""){
     alert("Les trois champs doivent être remplis");
 }
 else{
     document.getElementById("titre1").style.visibility="visible"
     document.getElementById("titre0").style.visibility="hidden"
+    console.log(Titre,Lien,Url)
     alire.insertAdjacentHTML("afterbegin",`<li>${Titre}<img src=${Url} alt="manga-cover"><i id=${Titre} 
     onClick=supprimer(this.id) class="fa-sharp fa-solid  fa-xmark"></i><a href=${Lien} target="blank">
-    <i class="fa-sharp fa-solid fa-book"></i></a><i id=${} onClick=marquerLu(this.id) class="fa-solid fa-check"></i></li>`);
+    <i class="fa-sharp fa-solid fa-book"></i></a><i id=${Titre} onClick=marquerLu(this.id) class="fa-solid fa-check"></i></li>`);
     document.getElementById('lien').value=""
     document.getElementById('titre').value=""
     document.getElementById('url').value=""
@@ -49,11 +51,10 @@ function supprimer(id){
 
 }
 function marquerLu(id){
-    console.log(id);
     
 
     document.getElementById("titre3").style.visibility="visible"
-    lu.insertAdjacentHTML("afterbegin",`<li>${id}<img src=${id} alt="manga-cover"><i id=${id} onClick=supprimer(this.id) class="fa-sharp fa-solid fa-xmark"></i></i><a href=${id} target="blank"><i class="fa-sharp fa-solid fa-book"></i></a></li>`);
+    lu.insertAdjacentHTML("afterbegin",`<li>${id}<i id=${id} onClick=supprimer(this.id) class="fa-sharp fa-solid fa-xmark"></i></i><a href=${id} target="blank"><i class="fa-sharp fa-solid fa-book"></i></a></li>`);
     supprimer(id);
 }
 
